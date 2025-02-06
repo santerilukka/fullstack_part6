0b2d597ddded8865d0695b04e36442f8ee1bdf49
santerilukka <156800715+santerilukka@users.noreply.github.com>
2025-02-06 14:18:00 +0200

    6.14 anekdootit ja backend, step1

--- redux-anecdotes/src/store.js
+++ redux-anecdotes/src/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit"
-import anecdoteReducer from "./reducers/anecdoteReducer"
+import anecdoteReducer, { setAnecdotes } from "./reducers/anecdoteReducer"
 import filterReducer from "./reducers/filterReducer"
 import notificationReducer from "./reducers/notificationReducer"
+import anecdoteService from "./services/anecdotes"
 
 const store = configureStore({
     reducer: {
@@ -10,5 +11,9 @@ const store = configureStore({
         notification: notificationReducer
     }
 })
+
+anecdoteService.getAll().then(anecdotes =>
+    store.dispatch(setAnecdotes(anecdotes))
+)
 
 export default store
