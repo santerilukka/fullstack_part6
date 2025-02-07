f7597d7bd725085334d9557002f505bd28491b91
santerilukka <156800715+santerilukka@users.noreply.github.com>
2025-02-07 15:28:02 +0200

    Tehtävä 6.20

---
+++ query-anecdotes/src/App.jsx
@@ -0,0 +1,52 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { getAnecdotes } from './requests/requests'
+
+const App = () => {
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes
+  })
+
+  if (result.isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (result.isError) {
+    return <div>anecdote service is not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
