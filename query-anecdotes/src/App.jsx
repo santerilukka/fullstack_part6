a3d8d6c128049efb104a58e5d03a1e545198a1c9
santerilukka <156800715+santerilukka@users.noreply.github.com>
2025-02-07 15:43:19 +0200

    6.21

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -2,7 +2,6 @@ import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
 import { getAnecdotes } from './requests/requests'
 
 const App = () => {
--- query-anecdotes/src/requests/requests.js
+++ query-anecdotes/src/requests/requests.js
@@ -4,3 +4,9 @@ export const getAnecdotes = async () => {
     const response = await axios.get("http://localhost:3001/anecdotes");
     return response.data;
 }
+
+export const createAnecdote = async (content) => {
+    const anecdote = { content, votes: 0 };
+    const response = await axios.post("http://localhost:3001/anecdotes", anecdote);
+    return response.data;
+}
